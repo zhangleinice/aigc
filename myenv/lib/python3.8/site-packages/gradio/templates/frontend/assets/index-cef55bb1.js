bfb0869a386cd5354ac687b932952d33238a9813
rocky <963754365@qq.com>
2023-08-01 17:49:05 +0800

    feat: Remove myenv/ directory from repository

--- myenv/lib/python3.8/site-packages/gradio/templates/frontend/assets/index-cef55bb1.js
+++
@@ -1,2 +0,0 @@
-import{C as e}from"./Column-b0174f8b.js";import"./index-7a480750.js";/* empty css                                             */const m=["static"];export{e as Component,m as modes};
-//# sourceMappingURL=index-cef55bb1.js.map
